89ce87b40f5fcc53b3de9a2720df900e0bcca220
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2017-05-23 16:25:17 -0700

    Part 1- Checkbox

---
+++ index.js
@@ -0,0 +1,41 @@
+class AppComponent extends React.Component{
+   constructor(props) {
+      super(props);
+
+      // underscore means that it's private
+      this._nextTodoId = 1;
+      this.state = {
+         filter: { showCompleted: true},
+         todos: [
+            {id: this._nextTodoId++, text: "Hey!", isCompleted: false},
+            {id: this._nextTodoId++, text: "Blabla", isCompleted: true},
+            {id: this._nextTodoId++, text: "Stuff", isCompleted: true},
+            {id: this._nextTodoId++, text: "Things!", isCompleted: false}
+         ]
+      };
+
+      this._onShowCompletedChanged = this._onShowCompletedChanged.bind(this);
+   }
+
+   render() {
+      const {filter} = this.state;
+
+      return(
+         <div>
+            <h2>Todo List</h2>
+            <label>
+               Show Completed
+               <input type="checkbox" checked={filter.showCompleted} onChange={this._onShowCompletedChanged} />
+            </label>
+         </div>
+      );
+   }
+
+   _onShowCompletedChanged(e) {
+      this.setState({
+         filter: {showCompleted: e.target.checked}
+      });
+   }
+}
+
+ReactDOM.render(<AppComponent />, document.getElementById("app"));
